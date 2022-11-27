31bbcaff29cedc72d53740d974b864e730388d3e
Andrex1999 <andresfelipemontiel99@hotmail.com>
2022-11-26 23:24:57 -0500

    Angular Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AsideComponent } from './Components/aside/aside.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { QuienesSomosComponent } from './Components/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './Components/servicios/servicios.component';
+import { TestimonioComponent } from './Components/testimonio/testimonio.component';
+import { ContactoComponent } from './Components/contacto/contacto.component';
+
+//Rutas
+const routes: Routes =[
+  {path: 'inicio', component: InicioComponent},
+  {path:"QuienesSomos",component:QuienesSomosComponent},
+  {path:"servicios",component:ServiciosComponent},
+  {path:"testimonio",component:TestimonioComponent},
+  {path:"contacto",component:ContactoComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AsideComponent,
+    InicioComponent,
+    QuienesSomosComponent,
+    ServiciosComponent,
+    TestimonioComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
 export class AppModule { }
